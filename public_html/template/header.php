c480061cd92cb9688009521d451c16c27e3fa99c
Yoshiki Hanashiro <e195733@ie.u-ryukyu.ac.jp>
2020-12-26 11:25:00 +0900

    基本の形を作成した

---
+++ public_html/template/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>さんぷるぺーじ</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Language" content="ja" />
+    <meta name="viewport" id="iphone-viewport" content="width=480, user-scalable=yes, maximum-scale=0.6667" />
+    <link rel = "stylesheet" type = "text/css" href = "global.css">
+    <script Langage="JavaScript">
+      function client_resizeTo(520, 480) {
+      　　if (document.layers) {
+      　　　　window.resizeTo(width, height);
+      　　　　window.resizeTo(width, height);
+      　　} else if (document.body.clientWidth) {
+      　　　　window.resizeTo(width*2, height*2);
+      　　　　var client_width = document.body.clientWidth;
+      　　　　var client_height = document.body.clientHeight;
+      　　　　window.resizeTo(width*3-client_width, height*3-client_height);
+      　　}
+      }
+    </script>
+  </head>
+  <body onload="createImageLayer();">
+    <a name="top"></a>
+    <div id="header">
+      <h1>さんぷるぺーじ</h1>
+      <p class="moto"><i>------ Sample</i><font color="red"> from </font><i>Miyazato Laboratory</i><p>
+    </div>
+ 	<td id="content">
+          <!main window!>
